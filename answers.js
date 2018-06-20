949e0a91e02bf359bd41a7d831078d46b265cfcf
Erica Bruenton <ercfit@gmail.com>
2018-06-20 16:43:43 -0600

    2nd commit - created some data structures and accessed some data

--- answers.js
+++ answers.js
@@ -1,11 +1,11 @@
-## Datatypes
-* Strings
-* Numbers
-* Booleans
-* Arrays
-* Objects
+// ## Datatypes
+// * Strings
+// * Numbers
+// * Booleans
+// * Arrays
+// * Objects
 
-### Answer the Following
+// ### Answer the Following
 //For each of the following, write which **datatypes** you would use to represent the data, and then give a small example of the **data structure**:
 
 // 1. A light switch that can be either on or off.
@@ -22,4 +22,24 @@
 	//array
 
 //**Commit1**
+
+//## Take it Easy
+
+//1. Make an array that holds all of the colors of the rainbow.
+	rainbow = ["red", "orange", "yellow", "green", "blue", "violet"];
+//2. Write code that will access "blue" from the rainbow array.
+	console.log(rainbow[4]);
+//3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+	erica = {
+			favorite_Food: "tacos",
+			hobby: "tennis",
+			city: "Castle Rock",
+			fav_datatype: "boolean"
+	};
+//4. Write code that will access your hobby from the object that you just created.
+	console.log(erica.hobby);
+
+
+//"2nd commit - created some data structures and accessed some data".
+
 
